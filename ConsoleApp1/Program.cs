58763c93f70d0d6154935639d49270c2c17ce82e
Marko Punčec <mpuncec46@gmail.com>
2025-05-02 13:28:28 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Models.Base;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Class1 cs = new Class1();
+
+            cs.ClassTest();
+        }
+    }
+}
---
+++ Models/Base/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Base
+{
+    public class Class1
+    {
+        public void ClassTest() {
+            Console.WriteLine("Class1 here!");
+        }
+    }
+}
