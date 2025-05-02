b4d326ccd304a96f2d392aac6d91f3c63984860d
Marko Punčec <mpuncec46@gmail.com>
2025-05-02 13:45:39 +0200

    Setting up stuff
    -MP

--- Models/Base/Class1.cs
+++ Models/Base/Class1.cs
@@ -1,9 +1,16 @@
-﻿namespace Models.Base
+﻿using Models.Interfaces;
+
+namespace Models.Base
 {
-    public class Class1
+    public class Class1 : IDeleteLater
     {
         public void ClassTest() {
             Console.WriteLine("Class1 here!");
+        }
+
+        public void RandomCrapPrinter()
+        {
+            Console.WriteLine("Random crap!");
         }
     }
 }
